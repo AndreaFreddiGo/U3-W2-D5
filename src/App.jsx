8b8bcb8eb5af45d3e7a563132c46391b5ba91083
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-12-11 19:50:21 +0100

    Adds corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/homepage.css'
 import './assets/styles/custom-bootstrap.css'
+import React, { useEffect, useState } from 'react'
 import UFooter from './components/UFooter'
 import UNavbar from './components/UNavbar'
 import UWeather from './components/UWeather'
 
 function App() {
+  const [coordinates, setCoordinates] = useState({ lon: null, lat: null })
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoordinates({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      })
+    })
+  }, [])
+
   return (
-    <>
-      <UNavbar />
-      <main>
-        <UWeather />
+    <div
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+    >
+      <UNavbar setCoordinates={setCoordinates} />
+      <main style={{ flex: 1 }}>
+        <UWeather latitude={coordinates.lat} longitude={coordinates.lon} />
       </main>
       <UFooter />
-    </>
+    </div>
   )
 }
 
--- src/components/UNavbar.jsx
+++ src/components/UNavbar.jsx
@@ -1,13 +1,32 @@
 import { Button, Col, Form, Navbar, Row } from 'react-bootstrap'
 import Logo from '../assets/images/Logo.png'
+import { useState } from 'react'
 
-const UNavbar = () => {
+const UNavbar = ({ setCoordinates }) => {
+  const [city, setCity] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c906786fcf8000ed9d78c6205f38333a`
+      )
+      const data = await response.json()
+      const { lon, lat } = data.coord
+      setCoordinates({ lon, lat })
+      setCity('')
+      console.log(lon, lat)
+    } catch (error) {
+      console.error('Error fetching the coordinates:', error)
+    }
+  }
+
   return (
     <Navbar className="justify-content-between px-1 px-md-3 py-3 bg-secondary-subtle shadow-lg">
       <Navbar.Brand href="#home" className="text-info ps-2 fs-4 fw-semibold">
         umbrellApp
       </Navbar.Brand>
-      <Form className="pe-3">
+      <Form className="pe-3" onSubmit={handleSubmit}>
         <Row>
           <Col xs="auto">
             <Form.Control
@@ -15,6 +34,8 @@ const UNavbar = () => {
               placeholder="You need me in your city?"
               className="py-2 border-info border-2 m-0 bg-transparent"
               variant="info"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
           </Col>
           <Col className="p-0">
